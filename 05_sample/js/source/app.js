553529e649198e262ed9cf682793b6c2e1cf0ad2
UltraBirdTech <ultrabirdrepti@gmail.com>
2018-03-19 19:15:13 +0900

    fix typo(s)

--- 05_sample/js/source/app.js
+++ 05_sample/js/source/app.js
@@ -2,11 +2,11 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Logo from '.components/Logo';
+import Logo from './components/Logo';
 
 ReactDOM.render(
   <h1>
-    <LOGO />アプリケーションにようこそ!
+    <Logo />アプリケーションにようこそ!
   </h1>,
   document.getElementById('app')
 );
