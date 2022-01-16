ae4265043ff53fdc886e17dea9d47ee3beeff356
UltraBirdTech <ultrabirdrepti@gmail.com>
2022-01-16 20:24:16 +0900

    add discover html and js

---
+++ 06_wine/js/source/discover.js
@@ -0,0 +1,17 @@
+'use strict';
+
+import Logo from './components/Logo';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+    <div stype {{padding: '20px'}}>
+        <h1> コンポーネント一覧 </h1>
+        <h2> Logo </h2>
+        <div style={{ display: 'inline-block', background: 'purple'}}>
+            <Logo />
+        </div>
+        // somthing write...
+    </div>
+    document.getElementById('pad')
+);
